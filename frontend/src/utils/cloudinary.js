87b5c43686a0f799d5906a14a67b0b4230d788d8
Arman Julfikar Khan <armaankks9017@gmail.com>
2026-04-26 03:36:58 +0530

    change upload preset to purechat_upload

--- frontend/src/utils/cloudinary.js
+++ frontend/src/utils/cloudinary.js
@@ -1,7 +1,7 @@
 export const uploadToCloudinary = async (file) => {
   const formData = new FormData();
   formData.append('file', file);
-  formData.append('upload_preset', 'chat'); // from user request
+  formData.append('upload_preset', 'purechat_upload'); // updated preset name
 
   try {
     const res = await fetch('https://api.cloudinary.com/v1_1/dz6qjy2t6/image/upload', {
